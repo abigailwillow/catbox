a8bc57cc25dcbc287ad012fbc2fa0a381ba55f9b
Abigail de Joode <contact@abbydiode.com>
2026-02-02 17:01:50 +0100

    Update userinfo.ts

--- src/commands/userinfo.ts
+++ src/commands/userinfo.ts
@@ -1,9 +1,42 @@
-command.linkCommand('userinfo', (msg, user) => {
-    let member = getMember(msg.guild, user)
+import { ChatInputCommandInteraction, EmbedBuilder, GuildMember, User } from "discord.js";
 
-    if (member != null) {
-        msg.channel.send(`**${member.displayName}** joined on \`${member.joinedAt.toISOString().substring(0, 10)}\``)
+export default function handle(interaction: ChatInputCommandInteraction) {
+    const mentionable = interaction.options.getMentionable('user');
+
+    if (mentionable instanceof User || mentionable instanceof GuildMember) {
+        const embed = new EmbedBuilder();
+
+        if (mentionable instanceof GuildMember) {
+            const member = mentionable ?? interaction.member as GuildMember;
+            const user = member.user;
+            
+            embed.setAuthor({ name: member.displayName, iconURL: member.displayAvatarURL({size: 32}) })
+            .setColor(member.displayHexColor);
+            embed.addFields([
+                {name: 'Discord ID', value: member.id, inline: true},
+                {name: 'Discord Tag', value: user.tag, inline: true},
+                {name: 'Account Creation Date', value: user.createdAt.toLocaleString(), inline: true},
+                {name: 'Join Date', value: member.joinedAt?.toLocaleString() ?? 'Unknown', inline: true},
+                {name: 'Bot', value: user.bot ? 'Yes' : 'No', inline: true},
+                {name: 'Roles', value: member.roles.cache.map(role => role.toString()).join(' '), inline: true},
+                {name: 'Avatar URL', value: user.displayAvatarURL({ size: 4096 }), inline: true}
+            ]);
+        } else {
+            const user = mentionable instanceof User ? mentionable : interaction.user;
+            
+            embed.setAuthor({ name: user.username, iconURL: user.displayAvatarURL({size: 32}) })
+            .setColor(user.hexAccentColor ?? null);
+            embed.addFields([
+                {name: 'Discord ID', value: user.id, inline: true},
+                {name: 'Discord Tag', value: user.tag, inline: true},
+                {name: 'Account Creation Date', value: user.createdAt.toLocaleString(), inline: true},
+                {name: 'Bot', value: user.bot ? 'Yes' : 'No', inline: true},
+                {name: 'Avatar URL', value: user.displayAvatarURL({ size: 4096 })}
+            ]);
+        }
+
+        interaction.reply({ embeds: [embed] });
     } else {
-        msg.channel.send(txt.err_no_user)
+        interaction.reply('Could not retrieve user info, did you mention a role by any chance?');
     }
-})
+};

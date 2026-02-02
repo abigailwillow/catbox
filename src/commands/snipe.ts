56a8f1d2f63ba2d321a1dc67b6c2f4151a054cac
Abigail de Joode <contact@abbydiode.com>
2026-02-02 17:25:03 +0100

    Comment currently unused commands

--- src/commands/snipe.ts
+++ src/commands/snipe.ts
@@ -1,45 +1,45 @@
-command.linkCommand('snipe', (msg, option) => {
-    let curSnipeArray = []
-    if (snipeArray.hasOwnProperty(msg.guild.id)) {
-        if (option === 'all') {
-            curSnipeArray = snipeArray[msg.guild.id]
-        } else if (option === 'clear') {
-            if (config.operators.includes(msg.author.id)) {
-                snipeArray[msg.guild.id] = []
-                msg.channel.send('Snipe cache was cleared.')
-            } else {
-                msg.channel.send(txt.err_no_operator)
-            }
-            return
-        } else {
-            for (let i = 0; i < snipeArray[msg.guild.id].length; i++) {
-                if (snipeArray[msg.guild.id][i].channel.id === msg.channel.id) {
-                    curSnipeArray.push(snipeArray[msg.guild.id][i])
-                }
-            }
-        }
-    }
+// command.linkCommand('snipe', (msg, option) => {
+//     let curSnipeArray = []
+//     if (snipeArray.hasOwnProperty(msg.guild.id)) {
+//         if (option === 'all') {
+//             curSnipeArray = snipeArray[msg.guild.id]
+//         } else if (option === 'clear') {
+//             if (config.operators.includes(msg.author.id)) {
+//                 snipeArray[msg.guild.id] = []
+//                 msg.channel.send('Snipe cache was cleared.')
+//             } else {
+//                 msg.channel.send(txt.err_no_operator)
+//             }
+//             return
+//         } else {
+//             for (let i = 0; i < snipeArray[msg.guild.id].length; i++) {
+//                 if (snipeArray[msg.guild.id][i].channel.id === msg.channel.id) {
+//                     curSnipeArray.push(snipeArray[msg.guild.id][i])
+//                 }
+//             }
+//         }
+//     }
 
-    if (curSnipeArray.length > 0) {
-        let embed = new EmbedBuilder()
-        .setColor(config.embedColor)
-        .setAuthor({ name: 'SNIPED! Here\'s a list of recently deleted messages.' })
-        .setTimestamp()
-        curSnipeArray.forEach(m => {
-            if (m != null) {
-                embed.addFields({
-                    name: `(${m.createdAt.toString().substr(16, 8)}) ${m.member.displayName} in #${m.channel.name}`,
-                    value: `${m.content}${m.edits && m.edits.length > 1 ? ` \`(edited)\`\n**Original message:**\n${m.edits[m.edits.length - 1].content}`: ''}`
-                    +`${m.attachments.size > 0 ? `\n**Attachment:** ${Array.from(m.attachments.values())[0].proxyURL}` : ''}\n\`ID: ${m.id}\``
-                })
-            }
-        })
+//     if (curSnipeArray.length > 0) {
+//         let embed = new EmbedBuilder()
+//         .setColor(config.embedColor)
+//         .setAuthor({ name: 'SNIPED! Here\'s a list of recently deleted messages.' })
+//         .setTimestamp()
+//         curSnipeArray.forEach(m => {
+//             if (m != null) {
+//                 embed.addFields({
+//                     name: `(${m.createdAt.toString().substr(16, 8)}) ${m.member.displayName} in #${m.channel.name}`,
+//                     value: `${m.content}${m.edits && m.edits.length > 1 ? ` \`(edited)\`\n**Original message:**\n${m.edits[m.edits.length - 1].content}`: ''}`
+//                     +`${m.attachments.size > 0 ? `\n**Attachment:** ${Array.from(m.attachments.values())[0].proxyURL}` : ''}\n\`ID: ${m.id}\``
+//                 })
+//             }
+//         })
 
-        msg.channel.send({ embeds: [embed] })
-    } else {
-        msg.channel.send("Whoops, I missed that.")
-    }
-})
+//         msg.channel.send({ embeds: [embed] })
+//     } else {
+//         msg.channel.send("Whoops, I missed that.")
+//     }
+// })
 
 // client.on('messageDelete', msg => {
 //     if (!snipeArray.hasOwnProperty(msg.guild.id)) {
--- src/commands/spawn.ts
+++ src/commands/spawn.ts
@@ -1,13 +1,13 @@
-command.linkCommand('spawn', (msg, member, amount) => {
-    if (member instanceof Map) {
-        member.forEach(m => {
-            changeBalance(m.id, amount)
-        })
+// command.linkCommand('spawn', (msg, member, amount) => {
+//     if (member instanceof Map) {
+//         member.forEach(m => {
+//             changeBalance(m.id, amount)
+//         })
 
-        msg.channel.send(`**Everyone** has received ${pluralize('cat', amount, true)}.`)
-    } else {
-        changeBalance(member.id, amount, _ => {
-            msg.channel.send(`**${member.displayName}** was granted ${pluralize('cat', amount, true)}.`)
-        })
-    }
-})
+//         msg.channel.send(`**Everyone** has received ${pluralize('cat', amount, true)}.`)
+//     } else {
+//         changeBalance(member.id, amount, _ => {
+//             msg.channel.send(`**${member.displayName}** was granted ${pluralize('cat', amount, true)}.`)
+//         })
+//     }
+// })
